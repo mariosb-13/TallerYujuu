35a5c7944686be196ad63f7ed6c7880a89fc5200
Mario <msanbar693a@ieslacampina.es>
2024-02-06 14:55:40 +0100

    js

--- index.php
+++ index.php
@@ -8,76 +8,106 @@ $resultado = $mysqli->query($sql);
 
 <!doctype html>
 <html lang="es">
+
+<head>
+	<!-- Required meta tags -->
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+	<!-- Bootstrap CSS -->
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+	<link rel="stylesheet" href="css/jquery.dataTables.min.css">
+
+	<!-- Optional JavaScript -->
+	<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+	<script src="js/jquery-3.4.1.min.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+	<script src="js/jquery.dataTables.min.js"></script>
+
+	<title>Club Deportivo La Venta</title>
+
+	<script>
+		$(document).ready(function() {
+			$('#tabla').DataTable();
+		});
+	</script>
+
+
+</head>
+
+<body>
+	<?php
+	require 'conexion.php';
+
+	$sql = "SELECT * FROM coche";
+
+	$resultado = $mysqli->query($sql);
+	?>
+
+	<!doctype html>
+	<html lang="es">
+
 	<head>
 		<!-- Required meta tags -->
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-		
+
 		<!-- Bootstrap CSS -->
 		<link rel="stylesheet" href="css/bootstrap.min.css">
 		<link rel="stylesheet" href="css/jquery.dataTables.min.css">
-		
+
 		<!-- Optional JavaScript -->
 		<!-- jQuery first, then Popper.js, then Bootstrap JS -->
-		<script src="js/jquery-3.4.1.min.js" ></script>
-		<script src="js/bootstrap.min.js" ></script>
-		<script src="js/jquery.dataTables.min.js" ></script>
-		
+		<script src="js/jquery-3.4.1.min.js"></script>
+		<script src="js/bootstrap.min.js"></script>
+		<script src="js/jquery.dataTables.min.js"></script>
+
 		<title>Club Deportivo La Venta</title>
-		
+
 		<script>
-			$(document).ready( function(){
+			$(document).ready(function() {
 				$('#tabla').DataTable();
 			});
 		</script>
-		
-		
+
+
 	</head>
+
 	<body>
 		<div class="container">
 			<div class="row">
-				<h1>Taller Yujuu</h1>
+				<h1>Socios</h1>
 			</div>
 			<br>
-			
+
 			<div class="row">
-			<a href="registrar.php"><button type="button" class="btn btn-primary">Registrar</button></a>
+				<a href="registrar.php"><button type="button" class="btn btn-primary">Registrar</button></a>
 			</div>
 			<br>
 			<br>
-			
-			<table id="tabla" class="display" style="width:100%">
-				<thead>
-					<tr>
-						<th>Nombre</th>
-						<th>Telefono</th>
-						<th>Fecha de nacimiento</th>
-						<th>Categoría</th>
-						<th></th>
-						<th></th>
-					</tr>
-				</thead>
-				<tbody>
-					<?php
-					 while($fila =$resultado->fetch_assoc()){
-						 echo "<tr>";
-							 echo "<td>$fila[nombre]</td>";
-							 echo "<td>$fila[telefono]</td>";
-							 echo "<td>$fila[fecha_nacimiento]</td>";
-							 echo "<td>$fila[categoria]</td>";
-							
-							 echo"<td><a href='editar.php?id=$fila[id]' class='btn btn-warning'>Editar</a></td>";
-							 echo"<td><a href='eliminar.php?id=$fila[id]' class='btn btn-danger'>Eliminar</a></td>";
-						 echo "</tr>";
-					 }
-					 $mysqli->close();
-					?>
-				</tbody>
-			</table>
-			
+
+			<div class="card">
+				<img class="card-img-top" src="/images/pathToYourImage.png" alt="Card image cap">
+				<div class="card-body">
+					<h4 class="card-title">Card title</h4>
+					<p class="card-text">
+						Some quick example text to build on the card title
+						and make up the bulk of the card's content.
+					</p>
+					<a href="#!" class="btn btn-primary">Go somewhere</a>
+				</div>
+			</div>
+
+
 		</div>
-	</div>
-	
-	
+		</div>
+
+
+	</body>
+
+	</html>
+
+
 </body>
+
 </html>
