c6e3b3fd65aab63d87ac0622e111369ddd4eb07d
Mario <msanbar693a@ieslacampina.es>
2024-02-06 14:28:37 +0100

    modificacion de index

--- index.php
+++ index.php
@@ -1,1 +1,83 @@
-yujuuu
+<?php
+require 'conexion.php';
+
+$sql = "SELECT * FROM coche";
+
+$resultado = $mysqli->query($sql);
+?>
+
+<!doctype html>
+<html lang="es">
+	<head>
+		<!-- Required meta tags -->
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		
+		<!-- Bootstrap CSS -->
+		<link rel="stylesheet" href="css/bootstrap.min.css">
+		<link rel="stylesheet" href="css/jquery.dataTables.min.css">
+		
+		<!-- Optional JavaScript -->
+		<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+		<script src="js/jquery-3.4.1.min.js" ></script>
+		<script src="js/bootstrap.min.js" ></script>
+		<script src="js/jquery.dataTables.min.js" ></script>
+		
+		<title>Club Deportivo La Venta</title>
+		
+		<script>
+			$(document).ready( function(){
+				$('#tabla').DataTable();
+			});
+		</script>
+		
+		
+	</head>
+	<body>
+		<div class="container">
+			<div class="row">
+				<h1>Taller Yujuu</h1>
+			</div>
+			<br>
+			
+			<div class="row">
+			<a href="registrar.php"><button type="button" class="btn btn-primary">Registrar</button></a>
+			</div>
+			<br>
+			<br>
+			
+			<table id="tabla" class="display" style="width:100%">
+				<thead>
+					<tr>
+						<th>Nombre</th>
+						<th>Telefono</th>
+						<th>Fecha de nacimiento</th>
+						<th>Categoría</th>
+						<th></th>
+						<th></th>
+					</tr>
+				</thead>
+				<tbody>
+					<?php
+					 while($fila =$resultado->fetch_assoc()){
+						 echo "<tr>";
+							 echo "<td>$fila[nombre]</td>";
+							 echo "<td>$fila[telefono]</td>";
+							 echo "<td>$fila[fecha_nacimiento]</td>";
+							 echo "<td>$fila[categoria]</td>";
+							
+							 echo"<td><a href='editar.php?id=$fila[id]' class='btn btn-warning'>Editar</a></td>";
+							 echo"<td><a href='eliminar.php?id=$fila[id]' class='btn btn-danger'>Eliminar</a></td>";
+						 echo "</tr>";
+					 }
+					 $mysqli->close();
+					?>
+				</tbody>
+			</table>
+			
+		</div>
+	</div>
+	
+	
+</body>
+</html>
