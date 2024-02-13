d49c1902360f205c5b11bae8be1ae78f2a55a704
Damián <dgarexp106a@ieslacampina.es>
2024-02-13 13:39:14 +0100

    añadimos css y js

--- coches.php
+++ coches.php
@@ -1,53 +1,56 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="es">
-    <head>
 
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-        
-      
-        <link rel="stylesheet" href="css/bootstrap.min.css">
-        
-        <title> Registrar coche </title>
-    </head>
-    <body>
-        <div class="container">
-            <div class="row">
-                <h1>Socios</h1>
-            </div>
-            
-            <div class="row">
-                <div class="col-md-8">
-                
-                    <form action="coches2.php" id="registro" name="registro" autocomplete="off" method="post">
-                        <div class="form-group">
-                      
-                            <label for="matricula"> Matrícula </label>
-                            <input type="number" name="matricula" id="matricula" class="form-control" placeholder="Introduce la matricula" required>
-                        </div>
-                        
-                        <div class="form-group">
-                           
-                            <label for="marca">Marca</label>
-                            <input type="text" name="marca" id="marca" class="form-control" placeholder="Introduce la marca" required>
-                        </div>
-                        
-                        <div class="form-group">
-                            
-                            <label for="modelo">Modelo</label>
-                            <input type="text" name="modelo" id="modelo" class="form-control" placeholder="Introduce el modelo" required>
-                        </div>
-                        
-                        <div class="form-group">
-                            <!-- Registrar -->
-                            <input type="submit" value="Registrar" class="btn btn-primary">
-                        </div>
-                    </form>
-                </div>
-            </div>
-        </div>
-        
-        <script src="js/jquery-3.4.1.min.js" ></script>
-        <script src="js/bootstrap.min.js" ></script>
-    </body>
+<head>
+	<!-- Required meta tags -->
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+	<!-- Bootstrap CSS -->
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+
+	<title>Taller Yujuu</title>
+</head>
+
+<body>
+	<div class="container">
+		<div class="row">
+			<h1>Taller Yujuu</h1>
+		</div>
+
+		<div class="row">
+			<h2>Registrar</h2>
+		</div>
+
+		<div class="row">
+			<div class="col-md-8">
+				<!-- Completar atributos de form -->
+				<form id="registro" name="registro" autocomplete="off" method="post" action="registrar2.php">
+					<div class="form-group">
+						<label for="nombre">Marca</label>
+						<input type="text" id="nombre" name="nombre" class="form-control" required>
+					</div>
+
+					<div class="form-group">
+						<label for="telefono">Modelo</label>
+						<input type="text" id="telefono" name="telefono" class="form-control" required>
+					</div>
+
+					<div class="form-group">
+						<label for="fecha_nac">Matricula</label>
+						<input type="date" id="fecha_nac" name="fecha_nac" class="form-control" required>
+					</div>
+
+					<div class="form-group">
+						<input type="submit" class="btn btn-primary" value="Registrar">
+					</div>
+				</form>
+			</div>
+		</div>
+	</div>
+
+	<script src="js/jquery-3.4.1.min.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+</body>
+
 </html>
