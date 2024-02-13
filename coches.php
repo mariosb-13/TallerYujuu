8a17bb98a25f5ad5715264b979b2b0cd88230f5b
Damián <dgarexp106a@ieslacampina.es>
2024-02-13 14:51:25 +0100

    Finalizamos

--- coches.php
+++ coches.php
@@ -1,27 +1,3 @@
-<<<<<<< HEAD
-<?php
-	require 'conexion.php';
-?>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Talleres Yuju</title>
-</head>
-<body>
-    <h1> Registrar Coche, $matrcicula </h1>
-    <hr><hr>
-    <form action="" method="post">
-            Matrícula: <input type="text" id="matricula" name="matricula"><br>
-            Marca: <input type="text" id="marca" name="marca"><br>
-            Modelo: <input type="text" id="modelo" name="modelo"><br>
-            Año: <input type="number" id="año" name="año"><br>
-            Color: <input type="color" id="color" name="color"><br>
-    </form>
-</body>
-=======
 <!DOCTYPE html>
 <html lang="es">
 
