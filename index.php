6e083b8855726a0e3fbf97885003396db38947ec
Mario <msanbar693a@ieslacampina.es>
2024-02-13 14:53:05 +0100

    nuevo index

--- index.php
+++ index.php
@@ -6,51 +6,72 @@ $sql = "SELECT * FROM coche";
 $resultado = $mysqli->query($sql);
 ?>
 
-<!DOCTYPE html>
+<!doctype html>
 <html lang="es">
-
-<head>
-	<!-- Required meta tags -->
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-	<!-- Bootstrap CSS -->
-	<link rel="stylesheet" href="css/bootstrap.min.css">
-	<link rel="stylesheet" href="css/jquery.dataTables.min.css">
-
-	<script src="js/jquery-3.4.1.min.js"></script>
-	<script src="js/bootstrap.min.js"></script>
-	<script src="js/jquery.dataTables.min.js"></script>
-
-	<title>Taller Yujuu</title>
-
-	<script>
-		$(document).ready(function() {
-			$('#tabla').DataTable();
-		});
-	</script>
-
-
-</head>
-
-<body>
-
-	<div class="container text-center">
-		<h1 class="text-center display-4">Taller Yujuu</h1>
-		<hr>
-
-		<body>
-			<form method="post" action="comprobacion.php">
-				<label for="matricula">Matricula:</label><br>
-				<input type="text" id="matricula" name="matricula" required><br>
-				<br>
-				<button type="submit" class="btn btn-success">Iniciar sesion</button>
-				<a href="coches.php"><button type="button" class="btn btn-primary">Registrar</button></a>
-			</form>
-
-		</body>
+	<head>
+		<!-- Required meta tags -->
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		
+		<!-- Bootstrap CSS -->
+		<link rel="stylesheet" href="css/bootstrap.min.css">
+		<link rel="stylesheet" href="css/jquery.dataTables.min.css">
+		
+		<!-- Optional JavaScript -->
+		<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+		<script src="js/jquery-3.4.1.min.js" ></script>
+		<script src="js/bootstrap.min.js" ></script>
+		<script src="js/jquery.dataTables.min.js" ></script>
+		
+		<title>Club Deportivo La Venta</title>
+		
+		<script>
+			$(document).ready( function(){
+				$('#tabla').DataTable();
+			});
+		</script>
+		
+		
+	</head>
+	<body>
+		<div class="container">
+			<div class="row">
+				<h1>Socios</h1>
+			</div>
+			<br>
+			
+			<div class="row">
+			<a href="registrar.php"><button type="button" class="btn btn-primary">Registrar</button></a>
+			</div>
+			<br>
+			<br>
+			
+			<table id="tabla" class="display" style="width:100%">
+				<thead>
+					<tr>
+						<th>Matricula</th>
+						<th>Marca</th>
+						<th>Modelo</th>
+					</tr>
+				</thead>
+				<tbody>
+					<?php
+					 while($fila =$resultado->fetch_assoc()){
+						 echo "<tr>";
+							 echo "<td>$fila[matricula]</td>";
+							 echo "<td>$fila[marca]</td>";
+							 echo "<td>$fila[modelo]</td>";
+							
+						 echo "</tr>";
+					 }
+					 $mysqli->close();
+					?>
+				</tbody>
+			</table>
+			
+		</div>
 	</div>
-
+	
+	
 </body>
-
 </html>
