8990941ff1b1582c2ad8b4fb6bcc19d2611b9c37
Damián <dgarexp106a@ieslacampina.es>
2024-02-13 13:25:09 +0100

    terminamos coches.php

--- coches.php
+++ coches.php
@@ -1,26 +1,53 @@
-<?php
-	require 'conexion.php';
-    $matricula=$_GET['matricula'];
-        $sql= "Select matricula from coches";
-        $res= $mysqli->query($sql);
-?>
+<!doctype html>
+<html lang="es">
+    <head>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Talleres Yuju</title>
-</head>
-<body>
-    <h1> Registrar Coche, $matrcicula </h1>
-    <hr><hr>
-    <form action="" method="post">
-            Matrícula: <input type="text" id="matricula" name="matricula"><br>
-            Marca: <input type="text" id="marca" name="marca"><br>
-            Modelo: <input type="text" id="modelo" name="modelo"><br>
-            Año: <input type="number" id="año" name="año"><br>
-            Color: <input type="color" id="color" name="color"><br>
-    </form>
-  </body>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        
+      
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+        
+        <title> Registrar coche </title>
+    </head>
+    <body>
+        <div class="container">
+            <div class="row">
+                <h1>Socios</h1>
+            </div>
+            
+            <div class="row">
+                <div class="col-md-8">
+                
+                    <form action="registrar2.php" id="registro" name="registro" autocomplete="off" method="post">
+                        <div class="form-group">
+                      
+                            <label for="Matrícula"> Matrícula </label>
+                            <input type="number" name="matricula" id="matricula" class="form-control" placeholder="Introduce la matricula" required>
+                        </div>
+                        
+                        <div class="form-group">
+                           
+                            <label for="marca">Marca</label>
+                            <input type="text" name="marca" id="marca" class="form-control" placeholder="Introduce la marca" required>
+                        </div>
+                        
+                        <div class="form-group">
+                            
+                            <label for="modelo">Modelo</label>
+                            <input type="text" name="modelo" id="modelo" class="form-control" placeholder="Introduce el modelo" required>
+                        </div>
+                        
+                        <div class="form-group">
+                            <!-- Registrar -->
+                            <input type="submit" value="Registrar" class="btn btn-primary">
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+        
+        <script src="js/jquery-3.4.1.min.js" ></script>
+        <script src="js/bootstrap.min.js" ></script>
+    </body>
 </html>
